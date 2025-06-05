60b393bb8560438e9f938a616f9c89c2c5d06a4c
ronyislam502 <ronyislam502@gmail.com>
2025-06-05 23:45:13 +0600

    app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import express from "express";
+import { Server } from "http";
 
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+let server:Server;
 
-app.listen(port, () => {
-  console.log(`Health-Care listening on port ${port}`)
-})
+const main=async()=>{
+    try {
+        server=app.listen(port, () => {
+         console.log(`Health-Care listening on port ${port}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
